11c6f64d7e2df2782e5a5cbbf3230c23598c322d
Franco Recalde <94661@sistemas.frc.utn.edu.ar>
2025-02-22 22:29:14 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/https://francorecalde.github.io/',
-  plugins: [react(),tailwindcss()],
-})
+  base: '/', // Usa una ruta relativa
+  plugins: [react(), tailwindcss()],
+});
